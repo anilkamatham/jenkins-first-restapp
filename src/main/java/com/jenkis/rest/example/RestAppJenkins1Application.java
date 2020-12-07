c2438a9e688bd63b4cc2a5c1f56b54edc3b71f85
anilkamatham <anil.kamatham@gmail.com>
2020-12-07 17:10:40 +0530

    war file

--- src/main/java/com/jenkis/rest/example/RestAppJenkins1Application.java
+++ src/main/java/com/jenkis/rest/example/RestAppJenkins1Application.java
@@ -2,12 +2,16 @@ package com.jenkis.rest.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+/**
+ * 
+ * @author anilk
+ *
+ */
 @SpringBootApplication
 public class RestAppJenkins1Application {
 
-	public static void main(String[] args) {
-		SpringApplication.run(RestAppJenkins1Application.class, args);
+	public static void main(final String[] args) {
+		SpringApplication.run(RestAppJenkins1Application.class, 	args);
 	}
 
 }
