79722a2b8ddff67318326e41ed47322413f00b08
anilkamatham <anil.kamatham@gmail.com>
2020-12-07 17:54:05 +0530

    changed

--- src/main/java/com/jenkis/rest/example/RestAppJenkins1Application.java
+++ src/main/java/com/jenkis/rest/example/RestAppJenkins1Application.java
@@ -2,6 +2,7 @@ package com.jenkis.rest.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 /**
  * 
  * @author anilk
@@ -9,10 +10,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * 
  */
 @SpringBootApplication
-public class RestAppJenkins1Application {
+public class RestAppJenkins1Application extends SpringBootServletInitializer{
 
 	public static void main(final String[] args) {
-		SpringApplication.run(RestAppJenkins1Application.class, 	args);
+		SpringApplication.run(RestAppJenkins1Application.class,	args);
 	}
 
 }
